f167125f7cddc865f11cc86d8a88c18a4ae56cbd
Aristotelis-Stratis <stratis.dev@outlook.com>
2024-07-09 21:25:14 +0200

    firebase integration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
 import { routes } from './app.routes';
+import { firebaseConfig } from './firebase.config';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+  ]
 };
