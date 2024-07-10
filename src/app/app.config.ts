8080d63a3cebe94cf249a69cc664392fb5974adb
tobiwall <t.w.95@web.de>
2024-07-10 12:14:59 +0200

    header

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,6 +6,8 @@ import { firebaseConfig } from './firebase.config';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getAnalytics, provideAnalytics, ScreenTrackingService } from '@angular/fire/analytics';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,6 +15,6 @@ export const appConfig: ApplicationConfig = {
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
-    provideDatabase(() => getDatabase()),
+    provideDatabase(() => getDatabase()), provideFirebaseApp(() => initializeApp({"projectId":"dabubble-7412a","appId":"1:522018485270:web:c36608a9b37a84f0e27137","storageBucket":"dabubble-7412a.appspot.com","apiKey":"AIzaSyBPcnhnxoL7P8Uxe4Lx7MfIbG1upnGqQPQ","authDomain":"dabubble-7412a.firebaseapp.com","messagingSenderId":"522018485270"})), provideAnalytics(() => getAnalytics()), ScreenTrackingService, provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideAnimationsAsync(),
   ]
 };
