64cdd59ace4eabae9c8d3d7b42f3d9d07627fe1a
tobiwall <t.w.95@web.de>
2024-07-10 21:08:39 +0200

    chat-channel.component

--- src/app/chat-channels/chat-channels.component.ts
+++ src/app/chat-channels/chat-channels.component.ts
@@ -1,12 +1,57 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-chat-channels',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './chat-channels.component.html',
   styleUrl: './chat-channels.component.scss'
 })
 export class ChatChannelsComponent {
 
+  menuChannelDropdown: boolean = true;
+  directMessages: boolean = true;
+  allChannels = [
+    {
+      name: "Entwickler Team",
+    },
+    {
+      name: "Office Team",
+    },
+    {
+      name: "Test Team",
+    }
+  ]
+
+  allDirectMessages = [
+    {
+      name: "Marco Amman",
+    },
+    {
+      name: "Sebastian Treittinger",
+    },
+    {
+      name: "Aristotelis Stratis",
+    },
+    {
+      name: "Tobias Wall",
+    }
+  ]
+
+  openMenuChannel() {
+    this.menuChannelDropdown = !this.menuChannelDropdown;
+  }
+
+  openDirectMessages() {
+    this.directMessages = !this.directMessages;
+  }
+
+  addNewChannel() {
+
+  }
+
+  addNewDirectMessage() {
+
+  }
 }
