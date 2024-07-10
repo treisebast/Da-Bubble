6e628eca4222f2d51c1821e672424b6a8644c885
Sebastian Treittinger <sebastian.treittinger@gmail.com>
2024-07-11 01:15:28 +0200

    bugfix

--- src/app/front-page/front-page.component.ts
+++ src/app/front-page/front-page.component.ts
@@ -5,12 +5,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-front-page',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, MatCardModule, ReactiveFormsModule, FormsModule, FooterComponent],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, MatCardModule, ReactiveFormsModule, FormsModule],
   templateUrl: './front-page.component.html',
   styleUrl: './front-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
