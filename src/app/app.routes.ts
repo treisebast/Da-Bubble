249bf4c1522df8de148f111d5215756083bdf844
Aristotelis-Stratis <stratis.dev@outlook.com>
2024-07-10 17:22:41 +0200

    front page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { FrontPageComponent } from './front-page/front-page.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: FrontPageComponent},
+    { path: '', component: MainContentComponent}
+];
