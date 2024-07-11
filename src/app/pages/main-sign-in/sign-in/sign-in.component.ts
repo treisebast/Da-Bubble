ca33ddec702539ce82ef880e367be151cc216558
Aristotelis-Stratis <stratis.dev@outlook.com>
2024-07-11 20:34:48 +0200

    sign-in validation alpha

--- src/app/pages/main-sign-in/sign-in/sign-in.component.ts
+++ src/app/pages/main-sign-in/sign-in/sign-in.component.ts
@@ -1,19 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, MatCardModule, ReactiveFormsModule, FormsModule, RouterModule],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, MatCardModule, ReactiveFormsModule, FormsModule, RouterModule, CommonModule],
   templateUrl: './sign-in.component.html',
-  styleUrl: './sign-in.component.scss'
+  styleUrls: ['./sign-in.component.scss']
 })
-export class SignInComponent {
 
+export class SignInComponent {
+  signInForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.signInForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  get email() {
+    return this.signInForm.get('email')!;
+  }
+
+  get password() {
+    return this.signInForm.get('password')!;
+  }
+
+  onSubmit() {
+    if (this.signInForm.valid) {
+      // Handle successful form submission here
+      console.log('Form Submitted', this.signInForm.value);
+      this.router.navigate(['/main']);
+    }
+  }
 }
