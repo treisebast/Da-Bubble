f7a1ecce9d948ec52700f02e9ec30dc9bac48d99
tobiwall <t.w.95@web.de>
2024-07-11 12:52:07 +0200

    chat-channel

--- src/app/chat-channels/chat-channels.component.ts
+++ src/app/chat-channels/chat-channels.component.ts
@@ -1,10 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddChannelComponent } from '../dialog-add-channel/dialog-add-channel.component';
 
 @Component({
   selector: 'app-chat-channels',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './chat-channels.component.html',
   styleUrl: './chat-channels.component.scss'
 })
@@ -12,6 +16,8 @@ export class ChatChannelsComponent {
 
   menuChannelDropdown: boolean = true;
   directMessages: boolean = true;
+  readonly dialog = inject(MatDialog);
+
   allChannels = [
     {
       name: "Entwickler Team",
@@ -27,15 +33,23 @@ export class ChatChannelsComponent {
   allDirectMessages = [
     {
       name: "Marco Amman",
+      imgScr: "./assets/img/profile/1.png",
+      online: true
     },
     {
       name: "Sebastian Treittinger",
+      imgScr: "./assets/img/profile/2.png",
+      online: true
     },
     {
       name: "Aristotelis Stratis",
+      imgScr: "./assets/img/profile/1.png",
+      online: false
     },
     {
       name: "Tobias Wall",
+      imgScr: "./assets/img/profile/4.png",
+      online: false
     }
   ]
 
@@ -48,10 +62,6 @@ export class ChatChannelsComponent {
   }
 
   addNewChannel() {
-
-  }
-
-  addNewDirectMessage() {
-
+    this.dialog.open(DialogAddChannelComponent);
   }
 }
