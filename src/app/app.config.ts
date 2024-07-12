5661a482a32c0e3b58753ea49e42f5ccee4e27a2
Sebastian Treittinger <sebastian.treittinger@gmail.com>
2024-07-12 21:50:29 +0200

    cleanup app.config.ts

---
+++ src/app/app.config.ts
@@ -0,0 +1,16 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { routes } from './app.routes';
+import { firebaseConfig } from './firebase.config';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ]
+};
