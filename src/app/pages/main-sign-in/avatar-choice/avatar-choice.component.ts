5788e4f87e9c075b960dcc6927c1aff13109a6f7
Aristotelis-Stratis <stratis.dev@outlook.com>
2024-07-13 13:57:14 +0200

    avatar file input and FileReader API implementation

--- src/app/pages/main-sign-in/avatar-choice/avatar-choice.component.ts
+++ src/app/pages/main-sign-in/avatar-choice/avatar-choice.component.ts
@@ -9,20 +9,21 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../shared/services/auth.service';
 import { UserService } from '../../../shared/services/user.service';
-import { User } from '../../../shared/models/user.model';
-import { firstValueFrom } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-avatar-choice',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, MatCardModule, ReactiveFormsModule, FormsModule, RouterOutlet, RouterModule],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, MatCardModule, ReactiveFormsModule, FormsModule, RouterOutlet, RouterModule],
   templateUrl: './avatar-choice.component.html',
   styleUrls: ['./avatar-choice.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AvatarChoiceComponent {
-  selectedAvatar: string = '/assets/img/front-page/avatar.svg';  // Default avatar image
+  selectedAvatar: string = '/assets/img/front-page/avatar.svg';
   @ViewChild('uploadedImage', { static: false }) uploadedImage!: ElementRef<HTMLImageElement>;
+  @ViewChild('fileInput', { static: false }) fileInput!: ElementRef<HTMLInputElement>;
+  fileError: string = '';
 
   constructor(
     private authService: AuthService,
@@ -33,12 +34,25 @@ export class AvatarChoiceComponent {
 
   selectAvatar(avatar: string) {
     this.selectedAvatar = avatar;
+    if (this.fileInput) {
+      this.fileInput.nativeElement.value = '';
+    }
     this.cdr.detectChanges();
   }
 
   onFileSelected(event: any) {
     const file = event.target.files[0];
     if (file) {
+      const fileType = file.type;
+      const validImageTypes = ['image/jpeg', 'image/png', 'image/svg+xml'];
+
+      if (!validImageTypes.includes(fileType)) {
+        this.fileError = 'Bitte nur JPG, PNG oder SVG Dateien hochladen.';
+        return;
+      } else {
+        this.fileError = '';
+      }
+
       const reader = new FileReader();
       reader.onload = (e: any) => {
         this.selectedAvatar = e.target.result;
@@ -49,12 +63,9 @@ export class AvatarChoiceComponent {
       };
       reader.readAsDataURL(file);
     }
-  } //FileReader API for reading hosted images
+  }
 
   logSelectedAvatar() {
     console.log('Selected Avatar:', this.selectedAvatar);
   }
-
-  // bei klick auf weiter, mit authService.getUser(). den authentifizierungsstatus abrufen und subscriben
-  // ....subscribe(user => {die logik})..., danach mit userService.updateUser den avatar updaten
 }
