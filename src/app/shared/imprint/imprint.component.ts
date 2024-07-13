b12836e5560bb041af99786694217962a4c8480d
Sebastian Treittinger <sebastian.treittinger@gmail.com>
2024-07-13 12:32:34 +0200

    design imprint

--- src/app/shared/imprint/imprint.component.ts
+++ src/app/shared/imprint/imprint.component.ts
@@ -1,18 +1,13 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, MatCardModule, FormsModule],
+  imports: [
+    MatCardModule,
+  ],
   templateUrl: './imprint.component.html',
-  styleUrl: './imprint.component.scss'
+  styleUrl: './imprint.component.scss',
 })
-export class ImprintComponent {
-
-}
+export class ImprintComponent {}
