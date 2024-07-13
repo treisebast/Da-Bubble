8ec945f06eb9b0d77be6c205f1eb4bf5023bd68e
Aristotelis-Stratis <stratis.dev@outlook.com>
2024-07-13 13:22:42 +0200

    avatar file input and FileReader API implementation

--- src/app/pages/main-sign-in/avatar-choice/avatar-choice.component.ts
+++ src/app/pages/main-sign-in/avatar-choice/avatar-choice.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewChild, ElementRef, ChangeDetectorRef } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -17,22 +17,44 @@ import { firstValueFrom } from 'rxjs';
   standalone: true,
   imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, MatCardModule, ReactiveFormsModule, FormsModule, RouterOutlet, RouterModule],
   templateUrl: './avatar-choice.component.html',
-  styleUrls: ['./avatar-choice.component.scss']
+  styleUrls: ['./avatar-choice.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AvatarChoiceComponent {
   selectedAvatar: string = '/assets/img/front-page/avatar.svg';  // Default avatar image
+  @ViewChild('uploadedImage', { static: false }) uploadedImage!: ElementRef<HTMLImageElement>;
 
   constructor(
     private authService: AuthService,
     private userService: UserService,
-    private router: Router
-  ) {}
+    private router: Router,
+    private cdr: ChangeDetectorRef
+  ) { }
 
   selectAvatar(avatar: string) {
     this.selectedAvatar = avatar;
+    this.cdr.detectChanges();
   }
 
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.selectedAvatar = e.target.result;
+        if (this.uploadedImage && this.uploadedImage.nativeElement) {
+          this.uploadedImage.nativeElement.src = e.target.result;
+          this.cdr.detectChanges();
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  } //FileReader API for reading hosted images
 
-  //bei klick auf weiter, mit authService.getUser(). den authentifizierungsstatus abrufen und subscriben ....subscribe(user => {die logik})..., danach mit userService.updateUser den avatar updaten
+  logSelectedAvatar() {
+    console.log('Selected Avatar:', this.selectedAvatar);
+  }
 
+  // bei klick auf weiter, mit authService.getUser(). den authentifizierungsstatus abrufen und subscriben
+  // ....subscribe(user => {die logik})..., danach mit userService.updateUser den avatar updaten
 }
