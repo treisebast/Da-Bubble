294cc57183c8a61f4d9b51c5e5ca09cd413c8bd3
Marco Ammann <marco-ammann@outlook.com>
2024-07-21 21:35:00 +0200

    fix: Ensure subscriptions are properly unsubscribed in HeaderComponent
    - Added ngOnDestroy method to HeaderComponent to properly unsubscribe from observables.
    - Managed subscriptions using a Subscription instance to avoid memory leaks.
    - Ensured that user data subscriptions are cleaned up when the component is destroyed.
    - Minor improvements to the readability and maintenance of the header component.

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, ViewChild, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit, OnDestroy } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -8,6 +8,7 @@ import { MenuComponent } from './menu/menu.component';
 import { AuthService } from '../services/auth.service';
 import { UserService } from '../services/user.service';
 import { User } from '../models/user.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -18,36 +19,48 @@ import { User } from '../models/user.model';
     MatInputModule,
     CommonModule,
     RouterModule,
-    MenuComponent
+    MenuComponent,
   ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent implements OnInit {
-
+export class HeaderComponent implements OnInit, OnDestroy {
   currentUser: Partial<User> = {
-    name: "",
-    avatar: "",
-    status: ""
+    name: '',
+    avatar: '',
+    status: '',
   };
+
+  showMenu = false;
+
+  private subs = new Subscription();
 
   constructor(private auth: AuthService, private userService: UserService) {}
 
-  ngOnInit() {
-    this.auth.getUser().subscribe(firebaseUser => {
-      if (firebaseUser?.uid) {
-        this.userService.getUser(firebaseUser.uid).subscribe(user => {
-          if (user) {
-            this.currentUser = user;
-          }
-        });
-      }
-    });
-  }
-
   @ViewChild(MenuComponent) MenuContent!: MenuComponent;
 
-  showMenu = false;
+
+  ngOnInit() {
+    const authSub = this.auth.getUser().subscribe((firebaseUser) => {
+      if (firebaseUser?.uid) {
+        const userSub = this.userService
+          .getUser(firebaseUser.uid)
+          .subscribe((user) => {
+            if (user) {
+              this.currentUser = user;
+            }
+          });
+        this.subs.add(userSub);
+      }
+    });
+    this.subs.add(authSub);
+  }
+
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
+  }
+
 
   toggleMenu() {
     console.log(this.showMenu);
