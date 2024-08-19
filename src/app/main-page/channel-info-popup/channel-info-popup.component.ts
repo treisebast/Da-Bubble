80fbee3eb684271907cac5b52d387f9f764005d6
Aristotelis-Stratis <stratis.dev@outlook.com>
2024-08-19 15:22:10 +0200

    channel info popup complete

--- src/app/main-page/channel-info-popup/channel-info-popup.component.ts
+++ src/app/main-page/channel-info-popup/channel-info-popup.component.ts
@@ -1,18 +1,58 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Import CommonModule for basic Angular directives
+import { CommonModule } from '@angular/common';
+import { ChannelService } from '../../shared/services/channel.service';
 import { Channel } from '../../shared/models/channel.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-channel-info-popup',
-  standalone: true, // Mark as standalone
-  imports: [CommonModule], // Import CommonModule if needed for *ngIf, *ngFor, etc.
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './channel-info-popup.component.html',
   styleUrls: ['./channel-info-popup.component.scss']
 })
 export class ChannelInfoPopupComponent {
   @Input() channel: Channel | null = null;
 
+  isEditingName = false;
+  isEditingDescription = false;
+  editedName: string = '';
+  editedDescription: string = '';
+
+  constructor(private channelService: ChannelService) {}
+
+  ngOnInit() {
+    if (this.channel) {
+      this.editedName = this.channel.name;
+      this.editedDescription = this.channel.description;
+    }
+  }
+
+  startEditingName() {
+    this.isEditingName = true;
+  }
+
+  startEditingDescription() {
+    this.isEditingDescription = true;
+  }
+
+  async saveName() {
+    if (this.channel) {
+      this.channel.name = this.editedName;
+      await this.channelService.updateChannel(this.channel);
+      this.isEditingName = false;
+    }
+  }
+
+  async saveDescription() {
+    if (this.channel) {
+      this.channel.description = this.editedDescription;
+      await this.channelService.updateChannel(this.channel);
+      this.isEditingDescription = false;
+    }
+  }
+
   closePopup() {
-    this.channel = null; // Reset channel to close the popup
+    this.channel = null;
   }
 }
