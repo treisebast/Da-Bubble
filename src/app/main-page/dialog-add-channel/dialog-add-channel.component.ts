cfd0390d2c23e0cc001c3cd48dc0b463dc99bbc9
Marco Ammann <marco-ammann@outlook.com>
2024-09-03 01:50:47 +0200

    Start work on adding users to channels during channel creation
    - Implemented a dialog that opens after channel creation, allowing users to add others to the newly created channel.
    - Added functionality to filter users by input text when the option to choose specific users is selected.
    - Users can be selected and deselected from the filtered list, and the selected users are stored in an array for later addition to the channel.

--- src/app/main-page/dialog-add-channel/dialog-add-channel.component.ts
+++ src/app/main-page/dialog-add-channel/dialog-add-channel.component.ts
@@ -2,13 +2,22 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef, MatDialogTitle, MatDialogModule } from '@angular/material/dialog';
+import {
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+  MatDialogModule,
+  MatDialog,
+} from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { Channel } from '../../shared/models/channel.model';
 import { ChannelService } from '../../shared/services/channel.service';
 import { AuthService } from '../../shared/services/auth.service';
+import { DialogAddUserComponent } from './dialog-add-user/dialog-add-user.component';
 
 @Component({
   selector: 'app-dialog-add-channel',
@@ -24,10 +33,11 @@ import { AuthService } from '../../shared/services/auth.service';
     MatDialogClose,
     MatProgressBarModule,
     CommonModule,
-    MatDialogModule
+    MatDialogModule,
+    DialogAddUserComponent
   ],
   templateUrl: './dialog-add-channel.component.html',
-  styleUrls: ['./dialog-add-channel.component.scss']
+  styleUrls: ['./dialog-add-channel.component.scss'],
 })
 export class DialogAddChannelComponent {
   loading = false;
@@ -39,7 +49,8 @@ export class DialogAddChannelComponent {
   constructor(
     public dialogRef: MatDialogRef<DialogAddChannelComponent>,
     private channelService: ChannelService,
-    private authService: AuthService
+    private authService: AuthService,
+    private dialog: MatDialog
   ) {
     this.subscribeToDialogEvents();
     this.setCurrentUser();
@@ -72,18 +83,47 @@ export class DialogAddChannelComponent {
   /**
    * Save the new channel
    */
-  async saveChannel() {
-    this.loading = true;
+  async createChannelAndGoToAddUsers() {
     const newChannel: Channel = this.createChannelObject();
+    try {
+      await this.saveChannelToFirbase(newChannel);
+    } catch (error) {
+      console.error('Error creating channel:', error);
+    } finally {
+      this.addUsersToChannel(newChannel);
+    }
+  }
+
+  /**
+   * Add users to the channel
+   * @param newChannel
+   */
+  addUsersToChannel(newChannel: Channel) {
+    const dialogRef = this.dialog.open(DialogAddUserComponent, {
+      data: { channel: newChannel },
+    });
+  }
+
+  async saveChannelToFirbase(newChannel: Channel) {
+    this.loading = true;
 
     try {
-      await this.channelService.addChannel(newChannel);
+      // await this.channelService.addChannel(newChannel);
+      await this.fakeDelay();
       this.dialogRef.close(newChannel);
     } catch (error) {
       console.error('Error adding channel:', error);
     } finally {
       this.loading = false;
     }
+  }
+
+  fakeDelay() {
+    return new Promise<void>((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 1000);
+    });
   }
 
   /**
