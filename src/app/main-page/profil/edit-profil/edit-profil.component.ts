cda3a676d10eb765164f59b8eb3e9c289bc177df
Sebastian Treittinger <sebastian.treittinger@gmail.com>
2024-10-24 02:11:31 +0200

    rework edit-profil-card and added validation

--- src/app/main-page/profil/edit-profil/edit-profil.component.ts
+++ src/app/main-page/profil/edit-profil/edit-profil.component.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import {
-  AbstractControl,
   FormControl,
   FormGroup,
   ReactiveFormsModule,
@@ -23,9 +22,21 @@ export class EditProfilComponent {
   @Input() isEditingEmail: boolean = false;
   @Output() closeEditProfil = new EventEmitter<boolean>();
 
-  editProfilForm = new FormGroup({
+  nameFormGroup = new FormGroup({
     name: new FormControl('', [Validators.minLength(3), Validators.required]),
+    nameRepeat: new FormControl('', [
+      Validators.minLength(3),
+      Validators.required,
+    ]),
+  });
+
+  emailFormGroup = new FormGroup({
     email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+      Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}'),
+    ]),
+    emailRepeat: new FormControl('', [
       Validators.required,
       Validators.email,
       Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}'),
@@ -36,37 +47,71 @@ export class EditProfilComponent {
 
   ngOnInit() {
     this.patchForm();
+    this.setupFieldValidation();
   }
 
-  //TODO: Add validation for email and repeat email, name and repeat name
   private patchForm() {
-    this.editProfilForm.patchValue({
+    this.nameFormGroup.patchValue({
       name: this.ownUser.name || '',
+    });
+    this.emailFormGroup.patchValue({
       email: this.ownUser.email || '',
     });
   }
 
-  get name(): AbstractControl {
-    return this.editProfilForm.get('name')!;
-  }
+  private setupFieldValidation() {
+    this.nameFormGroup.valueChanges.subscribe(() => {
+      const name = this.nameFormGroup.get('name')?.value;
+      const nameRepeat = this.nameFormGroup.get('nameRepeat')?.value;
+      if (name !== nameRepeat) {
+        this.nameFormGroup.get('nameRepeat')?.setErrors({ mismatch: true });
+      } else {
+        this.nameFormGroup.get('nameRepeat')?.setErrors(null);
+      }
+    });
 
-  get email(): AbstractControl {
-    return this.editProfilForm.get('email')!;
+    this.emailFormGroup.valueChanges.subscribe(() => {
+      const email = this.emailFormGroup.get('email')?.value;
+      const emailRepeat = this.emailFormGroup.get('emailRepeat')?.value;
+      if (email !== emailRepeat) {
+        this.emailFormGroup.get('emailRepeat')?.setErrors({ mismatch: true });
+      } else {
+        this.emailFormGroup.get('emailRepeat')?.setErrors(null);
+      }
+    });
   }
 
   onSubmitSave(): void {
-    if (this.editProfilForm.valid) {
+    if (this.isEditingName && this.nameFormGroup.valid) {
       this.userService.updateUser({
         ...this.ownUser,
-        ...this.editProfilForm.value,
+        name: this.nameFormGroup.value.name,
       } as User);
-      this.editProfilForm.reset();
+      this.nameFormGroup.reset();
+      this.closeEditProfil.emit(false);
+    }
+
+    if (this.isEditingEmail && this.emailFormGroup.valid) {
+      this.userService.updateUser({
+        ...this.ownUser,
+        email: this.emailFormGroup.value.email,
+      } as User);
+      this.emailFormGroup.reset();
       this.closeEditProfil.emit(false);
     }
   }
 
   cancel(): void {
-    this.editProfilForm.reset();
+    this.nameFormGroup.reset();
+    this.emailFormGroup.reset();
     this.closeEditProfil.emit(false);
+  }
+
+  get nameControls() {
+    return this.nameFormGroup.controls;
+  }
+
+  get emailControls() {
+    return this.emailFormGroup.controls;
   }
 }
