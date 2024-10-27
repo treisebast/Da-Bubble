28f28adf118df309e781e6f8f9af20b44df45d19
Marco Ammann <marco-ammann@outlook.com>
2024-10-27 17:40:01 +0100

    implementet auth.guard

---
+++ src/app/shared/guards/auth.guard.ts
@@ -0,0 +1,21 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { map, take } from 'rxjs/operators';
+import { AuthService } from '../services/auth.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  return authService.getUser().pipe(
+    take(1),
+    map(user => {
+      if (user) {
+        return true;
+      } else {
+        router.navigate(['/login']);
+        return false;
+      }
+    })
+  );
+};
